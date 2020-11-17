9dbf6d51795d9b48bab6a0f24475b463dd4f5f20
bramos013 <brunoramos013@gmail.com>
2020-11-16 21:48:29 -0300

    Subindo Server.JS e arquivo de automaçao

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.write('Aplicação NodeJS Rodando!');
+    res.end();
+}).listen(3000);
+console.log('Servidor Iniciando');
