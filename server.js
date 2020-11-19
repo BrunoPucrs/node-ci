3692ca80e468b03eb40d42de1688d4b89d264819
bramos013 <brunoramos013@gmail.com>
2020-11-19 19:36:59 -0300

    Bug2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var http = require('http')
+var http = require
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
-    res.write('Aplicação NodeJS Rodando!')
+    res.write('Aplicação NodeJS Rodando!');
     res.end();
 }).listen(3000);
 console.log('Servidor Iniciando');
